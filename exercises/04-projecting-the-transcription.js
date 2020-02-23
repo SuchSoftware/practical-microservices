f2c6c726ef61c115f9932069ec910d0ba5fce0fb
Ethan Garofolo <ethan@suchsoftware.com>
2020-02-23 14:55:12 -0500

    Add TODO to exercise

--- exercises/04-projecting-the-transcription.js
+++ exercises/04-projecting-the-transcription.js
@@ -2,6 +2,7 @@ const uuid = require('uuid/v4')
 
 const { project } = require('../src/message-store/read')
 const TranscribedControls = require('../src/transcribe-component/controls/events/transcribed')
+// TODO: Fill out the projection required here
 const projection = require('../src/transcribe-component/projection')
 
 const events = [TranscribedControls.example()]
